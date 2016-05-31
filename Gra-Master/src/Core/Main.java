39441a6c44583e33af1e69b423cfe6e61ebaa513
kalwador <szpila@piotrek@gmail.com>
2016-05-31 22:19:33 +0200

    31
    31

--- Gra-Master/src/Core/Main.java
+++ Gra-Master/src/Core/Main.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gra.master;
+package Core;
+
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -13,11 +9,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author kalvador
- */
-public class GraMaster extends Application {
+
+public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
@@ -34,16 +27,13 @@ public class GraMaster extends Application {
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 720, 480);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
