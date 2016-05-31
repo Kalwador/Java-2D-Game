3c59987df0a2dc328a7ba6c663735a5d8f592fde
kalwador <szpila@piotrek@gmail.com>
2016-05-31 23:45:21 +0200

    312
    312

--- Gra-Master/src/Core/Main.java
+++ Gra-Master/src/Core/Main.java
@@ -1,41 +1,58 @@
 package Core;
 
-
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 720, 480);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
+    Stage window;
+    Scene menu, mainScene;
+    
     public static void main(String[] args) {
         launch(args);
     }
     
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        
+        Label label1 = new Label("Menu");
+        
+        Button button1 = new Button("Graj");
+        button1.setOnAction(e -> window.setScene(mainScene));
+         
+        
+        //layout1 
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
+        menu = new Scene(layout1,200,200);
+        
+        
+        //button 2
+        Button button2 = new Button("do menu");
+        button2.setOnAction(e -> window.setScene(menu));
+    
+    
+        //layout2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        mainScene = new Scene(layout2,600,300);
+        
+        window.setScene(menu);
+        window.setTitle("gra");
+        window.show();
+    }
 }
