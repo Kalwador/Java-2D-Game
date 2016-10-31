7afc4ac407d44a41a82c7b1b656eef542dfaf33e
kalwador <szpila@piotrek@gmail.com>
2016-10-31 17:21:00 +0100

    Sterowanie w teście Slick2D

--- GameSlave/src/Core/MainLoop.java
+++ GameSlave/src/Core/MainLoop.java
@@ -4,27 +4,53 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class MainLoop extends BasicGame
 {
     private TiledMap map;
+    private Image img;
+    private float x,y;
+    //private Image heroPath = new Image("Resources/Graphics/Character/Facet4.png",true); //test do postci
+
     
     @Override
-    public void init(GameContainer container) throws SlickException{
+    public void init(GameContainer gc) throws SlickException{
+        //info: tutaj inicjalizujemy wszystkie obiekty które się pojawią
         map = new TiledMap("Resources/graphics/maps/map.tmx");
+        img = new Image("Resources/Graphics/Character/Facet4.png");
+        
+        x = 400;
+        y = 400;
+        
     }
  
     @Override
-    public void update(GameContainer container, int delta) throws SlickException{
+    public void update(GameContainer gc, int delta) throws SlickException{
+        //info: tutaj kalkulacje, movement itp
         int objectLayer = map.getLayerIndex("Object");
         
-        //map.getTileId(0, 0, objectLayer);
+        Input input = gc.getInput();
+        
+        if(input.isKeyDown(Input.KEY_W)) {y -= 0.2 * delta;}
+        if(input.isKeyDown(Input.KEY_S)) {y += 0.2 * delta;}
+        if(input.isKeyDown(Input.KEY_A)) {x -= 0.2 * delta;}
+        if(input.isKeyDown(Input.KEY_D)) {x += 0.2 * delta;}
+        
+        
+        
     }
  
-    public void render(GameContainer container, Graphics g) throws SlickException{
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException{
+        //info: tutaj wyświtlamy to co wyliczyliśmy w update
         map.render(0, 0);
+        img.draw(x,y,x+48,y+48,0,0,48,48); 
+        //(startXonWindow,startYonWindow,endXonWindow,endYonWindow,
+        //  startXpartOfpicture,startYpartOfpicture,endXpartOfpicture,endYpartOfpicture)
     }
     
     
@@ -36,9 +62,10 @@ public class MainLoop extends BasicGame
     {
         try
         {
-            AppGameContainer app = new AppGameContainer(new MainLoop());
-            app.setDisplayMode(1000, 1000, false);
-            app.start();
+            AppGameContainer window = new AppGameContainer(new MainLoop());
+            window.setDisplayMode(1000, 1000, false);
+            window.setTargetFrameRate(150);
+            window.start();
         }
         catch (SlickException e)
         {
