3863ab2e75a9510f1feabedebb0aacfcc6fb00a9
kalwador <szpila@piotrek@gmail.com>
2016-11-01 21:02:16 +0100

    n n

--- ProjektGry2D/src/Core/ActionHandler.java
+++ ProjektGry2D/src/Core/ActionHandler.java
@@ -1,7 +1,7 @@
 /*
     Obsługa wszystkich zdarzeń, klawiatury, myszki,
  */
-package Core;
+package core;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
@@ -15,10 +15,10 @@ import org.newdawn.slick.tiled.TiledMap;
 public class ActionHandler {
     float gameSpeed; //przyda się do konsoli
     public static void handle(Input input,int delta) {
-        if(input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_UP)) {MainLoop.y -= 0.2 * delta;}
-        if(input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN)) {MainLoop.y += 0.4 * delta;}
-        if(input.isKeyDown(Input.KEY_A) || input.isKeyDown(Input.KEY_LEFT)) {MainLoop.x -= 0.2 * delta;}
-        if(input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT)) {MainLoop.x += 0.4 * delta;}
+        if(input.isKeyDown(Input.KEY_W) || input.isKeyDown(Input.KEY_UP)) {Play.y -= 0.2 * delta;}
+        if(input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN)) {Play.y += 0.4 * delta;}
+        if(input.isKeyDown(Input.KEY_A) || input.isKeyDown(Input.KEY_LEFT)) {Play.x -= 0.2 * delta;}
+        if(input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT)) {Play.x += 0.4 * delta;}
         if(input.isKeyPressed(Input.KEY_ESCAPE)) {/*metoda otwierajaca menuPause*/}
     }
 }
--- ProjektGry2Dv2/src/core/actor/Race.java
+++
@@ -1,5 +0,0 @@
-package core.actor;
-
-public interface Race {
-    
-}
